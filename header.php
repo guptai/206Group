8e2cb2175790888388b045861a3e4e7ce36e2415
guptai <guptai@umich.edu>
2015-03-24 16:15:20 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+	<h1> Ann Arbor Social Scene (Placeholder) </h1>
+	<nav id="sidebar"> 
+		<ul class="menubar">
+			<li> <a href="index.html"> Home </a></li>
+			<li> <a href="bars.html"> Bars </a></li>
+			<li> <a href="restaurants.html">  Restaurants </a></li>
+			<li> <a href="sports.html"> Sporting Events </a></li>
+			<li> <a href="form.html"> Feedback </a></li>
+		</ul>
+	</nav>
+<header>
